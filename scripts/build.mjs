7017b1c0a258c81cdcc8bf9b37290f53e934dfcd
jh.ryu <jh.ryu@quintet.co.kr>
2024-10-04 18:14:17 +0900

    :construction_worker: Edit Build Script

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,10 +1,27 @@
+import fs from 'fs/promises'
+import path from 'path'
 import * as esbuild from 'esbuild'
+
+let DEBUG_FLAG = process.argv[2]
+const sPath = path.join(process.cwd(), 'setting.json')
+
+if (DEBUG_FLAG === '--product') {
+	DEBUG_FLAG = false
+
+	await fs.rm(sPath)
+	await fs.writeFile(sPath, JSON.stringify({ logLevel: 'info' }, null, 2))
+} else {
+	DEBUG_FLAG = true
+
+	await fs.rm(sPath)
+	await fs.writeFile(sPath, JSON.stringify({ logLevel: 'debug' }, null, 2))
+}
 
 await esbuild
 	.build({
 		entryPoints: ['src/app.ts'],
 		outfile: 'dist/app.js',
-		logLevel: 'info',
+		logLevel: DEBUG_FLAG ? 'debug' : 'info',
 		bundle: true,
 		minify: true,
 		treeShaking: true,
